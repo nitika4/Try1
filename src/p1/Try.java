72804d54b6eb708313f87e531c92873108d62dec
nitika4 <nitika4.agarwal@gmail.com>
2017-07-07 11:42:56 +0800

    simple out

---
+++ src/p1/Try.java
@@ -0,0 +1,15 @@
+package p1;
+
+public class Try {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Improvosong the Code reviews");
+		
+	}
+
+}
